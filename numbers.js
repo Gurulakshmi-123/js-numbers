de88e1585e26275f8497c5f5004f8244bf60969d
Gurulakshmi-123 <gurulakshmi2897@gmail.com>
2022-08-16 19:00:54 +0530

    javascript numbers concept

---
+++ numbers.js
@@ -0,0 +1,16 @@
+var x = 999999999999999;
+var y = 999999999999999;
+var z = x+y;
+console.log(z);
+
+
+var x = 0.2 + 0.1;
+console.log(x);
+
+var x = (0.2 * 10 + 0.1 * 10) / 10;
+console.log(x);
+
+var x = 10;
+var y = 20;
+var z = x + y;
+console.log(z);
